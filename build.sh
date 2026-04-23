7c56147947e6adca2408ab141107b31942b0adde
Crazy_ArKzX <jshwjjejdj@gmail.com>
2026-04-23 20:21:42 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if ! command -v sox &> /dev/null; then
-    sudo apt install -y sox
+    apt install -y sox
 fi
 
 if ! command -v wavpack &> /dev/null; then
-    sudo apt install -y wavpack
+    apt install -y wavpack
 fi
 
 if ! command -v gcc &> /dev/null; then
-    sudo apt install -y gcc
+    apt install -y gcc
 fi
 
 clang -w src/main.c -o bin/main && cd bin && ./main
