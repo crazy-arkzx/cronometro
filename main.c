d0e43461b3402a44da140c9bdeae111a7b71c824
Crazy_ArKzX <jshwjjejdj@gmail.com>
2026-04-23 18:38:00 -0300

    update main.c

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main() 
 {
     int sec = 0;
     const int timemax = 60 * 3600;
-    
+
     while(1) 
     {
         int hora = sec / 3600;
         int min = (sec % 3600) / 60;
         int seg = sec % 60;
-        
+
         if(hora > 0) printf("\r%d:%02d:%02d", hora, min, seg);
         else if(min > 0) printf("\r%d:%02d", min, seg);
         else printf("\r%d", seg);
-        
+
         fflush(stdout);
-        
+
         if(hora == 0 && min == 1 && seg == 0)
         {
             system("play -q sounds/end.wav");
@@ -30,9 +30,8 @@ int main()
             system("play -q sounds/max.wav");
             break;
         }
-        
-        clock_t inicio = clock();
-        while((double)(clock() - inicio) / CLOCKS_PER_SEC < 1.0);
+
+        sleep(1);
         sec++;
     }
     return 0;
