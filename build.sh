fa9378b366ef9069e4c6edcb6aef081269533600
Crazy_ArKzX <jshwjjejdj@gmail.com>
2026-04-30 01:04:43 -0300

    Link pthread, math, and dynamic libraries in build script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ if ! command -v wavpack &> /dev/null; then
     apt install -y wavpack
 fi
 
-clang -w src/main.c -o bin/main && cd bin && ./main
+clang -w src/main.c -o bin/main -lpthread -lm -ldl && cd bin && ./main
